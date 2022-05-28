137c93e9cd9a81f70b46fa1dbc91eaee68a18fa2
yunsangjun <alajillo2021@gmail.com>
2022-05-28 19:25:58 +0900

    add javascript version event handle

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, throttleTime, scan } from 'rxjs';
 
+const app = document.getElementById('app') as HTMLDivElement;
+const buttonElement = document.createElement('button');
+buttonElement.textContent = '버튼';
+app.append(buttonElement);
 
-const buttonElement = document.getElementById('button') as HTMLButtonElement;
+// buttonElement.addEventListener('click', () => {
+//   console.log('clicked');
+// });
 
-fromEvent(buttonElement,'click').subscribe(()=> console.log('clicked'))
+// fromEvent(buttonElement, 'click').subscribe((event) => console.log('clicked'));
+
+fromEvent(buttonElement, 'click')
+  .pipe(
+    throttleTime(1000),
+    scan((count) => count + 1, 0)
+  )
+  .subscribe((count) => console.log(`Clicked ${count} times`));
